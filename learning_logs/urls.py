dd3ea2e755aeeb4a115d1c4db439afd49831ed24
蔡潮森 <>
2019-09-03 17:19:49 +0800

    add entry

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -11,4 +11,8 @@ urlpatterns = [
     path('topics/',views.topics,name='topics'),
     # 主题条目
     re_path(r'^topics/(?P<topic_id>\d+)/$',views.topic,name='topic'),
+    # 添加新主题的网页
+    re_path(r'^new_topic/$',views.new_topic,name='new_topic'),
+    # 添加新学习条目
+    re_path(r'^new_entry/(?P<topic_id>\d+)/$',views.new_entry,name='new_entry'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from learning_logs.models import Topic
+from .forms import TopicForm,EntryForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -18,3 +21,32 @@ def topic(request,topic_id):
     entries = topic.entry_set.order_by('-date_added')   # 降序排序
     context = {'topic':topic,'entries':entries}
     return render(request,'learning_logs/topic.html',context)
+
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+        context = {'form': form}
+        return render(request,'learning_logs/new_topic.html',context)
+    else:
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+
+
+def new_entry(request,topic_id):
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        form = EntryForm()
+        context = {'form': form,
+                   'topic': topic,}
+        return render(request,'learning_logs/new_entry.html',context)
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic',args=[topic_id]))
